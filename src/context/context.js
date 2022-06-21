b6a2478dcfc6f3d3245e3b779fe4d9c14e547801
tmgAzit <azit.ghising@gmail.com>
2022-06-21 21:10:53 +0930

    Info and Card Component is define.

--- src/context/context.js
+++ src/context/context.js
@@ -3,6 +3,7 @@ import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
+import { useContext } from 'react';
 
 const rootUrl = 'https://api.github.com';
 const GithubContext = React.createContext();
@@ -10,9 +11,20 @@ const GithubContext = React.createContext();
 //Provider, Consumer - githubContex.provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [githubRepos, setGithubRepos] = useState(mockRepos);
+  const [githubFollowers, setGithubFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+    <GithubContext.Provider
+      value={{ githubUser, githubRepos, githubFollowers }}
+    >
+      {children}
+    </GithubContext.Provider>
   );
+};
+
+export const useGlobalContext = () => {
+  return useContext(GithubContext);
 };
 
 export { GithubProvider, GithubContext };
